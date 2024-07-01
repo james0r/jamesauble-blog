302096df03ac6983e0134fcb5300a97b60898ffc
James <james.auble@gmail.com>
2024-07-01 08:45:26 -0700

    added tooltip and colocated with button to get auto vanishing after click

--- src/components/react/Tooltip.tsx
+++ src/components/react/Tooltip.tsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Tooltip as ReactTooltip } from 'react-tooltip'
+import { clsx } from 'clsx'
 
-const Tooltip = () => {
+const Tooltip = ({ url, tag }) => {
+  const [showTooltip, setShowTooltip] = React.useState(false)
+  const id = React.useId()
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowTooltip(false)
+    }, 2000)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [showTooltip])
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    navigator.clipboard.writeText(url)
+    setShowTooltip(true)
+  }
+
   return (
-    <div>Tooltip</div>
+    <>
+      <ReactTooltip
+        id={id}
+        place="top"
+        content="Copied!"
+        isOpen={showTooltip}
+      />
+      <button
+        type="button"
+        data-tooltip-id={id}
+        data-tooltip-variant="light"
+        data-url={url}
+        className={clsx('heading-anchor', tag)}
+        onClick={handleClick}
+      >
+        #
+      </button>
+    </>
   )
 }
 
